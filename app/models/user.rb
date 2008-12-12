a5abf0f03748c92629e2d9df1e0ac64c176241cb
ferio <ant_on1988@mail.ru>
2008-12-12 13:49:30 +0300

    correcting

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+﻿class User < ActiveRecord::Base
+   validates_presence_of :login, :password
+  validates_length_of :password, :within => 3..20
+  validates_uniqueness_of :login
+  
+  has_many :posts
+end
